10f494f3f53f6659c1b3ed934e15d38eb941fd05
Mun Kyoung-min <mun9659@naver.com>
2024-07-07 15:54:04 +0900

    third world init

---
+++ backend/thirdworld/src/main/java/com/games/thirdworld/ThirdworldApplication.java
@@ -0,0 +1,13 @@
+package com.games.thirdworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdworldApplication.class, args);
+	}
+
+}
---
+++ backend/thirdworld/src/test/java/com/games/thirdworld/ThirdworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.thirdworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
